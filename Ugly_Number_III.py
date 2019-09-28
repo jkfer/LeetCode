e7d260c37ecfd60c9004420f109b17ca3070c1c1
Joseph Fernando <jkfer@mx>
2019-09-28 07:55:08 -0400

    - completing problem. working solution

--- Ugly_Number_III.py
+++ Ugly_Number_III.py
@@ -22,40 +22,55 @@ Example 4:
 Input: n = 1000000000, a = 2, b = 217983653, c = 336916467
 Output: 1999999984
 
+Constraints:
+
+1 <= n, a, b, c <= 10^9
+1 <= a * b * c <= 10^18
+It's guaranteed that the result will be in range [1, 2 * 10^9]
+
+
 Hint:
 Write a function f(k) to determine how many ugly numbers smaller than k. As f(k) is non-decreasing, try binary search.
 
+# LCM - lowest common multiplier of a and b
+The lowest number that is devisible by both a and b
+this is calculated by = a*b / gcd(a, b)
+
+# GCD - greatest common denominator of a and b
+The biggest number that can divide a and b without any remander
+
 """
 
-
+from fractions import gcd
 
 class Solution:
-    # return how many ugly numbers are smaller than k
-    def count_ugly(self, start, end, a, b, c):
-        count = 0
-        while start <= end:
-            if start % a == 0 or start % b == 0 or start % c == 0:
-                count += 1
-            start += 1
+    def lcm(self, a, b):
+        return a*b // gcd(a, b)
 
-        return count
+    # return how many ugly numbers are smaller or equal to k
+    def count_ugly(self, k, a, b, c, ab, bc, ac, abc):
+        ans = k // a + k // b + k // c - (k // ab + k // bc + k // ac) + k // abc
+        return ans
     
-    def basecheck(self, n, end, a, b, c):
-        # return True if there could be an ugly number within the given range:
-        pass
-    
-    def generate_seq(self, start, end, a, b, c):
-        K = a*b*c
-        L = []
-        for l in [a, b, c]:
-            L += range(start, K+1, l)
-        
-        return list(set(L))
-
 
     def nthUglyNumber(self, n, a, b, c):
-        # generate the seq:
-        L = self.generate_seq(a, b, c)
+        ab, bc, ac = self.lcm(a, b), self.lcm(b, c), self.lcm(a, c)
+        abc = self.lcm(ab, c)
+        # It's guaranteed that the result will be in range [1, 2 * 10^9]
+
+        start = 1
+        end = 2 * 10 ** 9
+
+        while start < end:
+            H = start + (end - start) // 2
+            if self.count_ugly(H, a, b, c, ab, bc, ac, abc) < n:
+                start = H + 1
+            else: # case where mid number H has smaller ugly numbers than n
+                # you have to move mid to the right then
+                end = H
         
-        
-        
+        return start
+                
+S = Solution()
+a = S.nthUglyNumber(1000000000, 2, 217983653, 336916467)
+print(a)
