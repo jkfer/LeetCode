b3a736fd7f5e57e1b1c5b4e1280869e90cf59657
Joseph Fernando <jkfer@mx>
2019-10-27 17:18:13 -0400

    working medium

--- Integer_to_Roman.py
+++ Integer_to_Roman.py
@@ -2,7 +2,6 @@
 
 from math import pow
 
-
 class Solution:
     D = {
         1: "I",
@@ -11,15 +10,36 @@ class Solution:
         50: "L",
         100: "C",
         500: "D",
-        1000: "M"
+        1000: "M", 
+        4: "IV",
+        9: "IX",
+        40: "XL",
+        90: "XC",
+        400: "CD",
+        900: "CM"
     }
 
     def intToRoman(self, num):
         ans = []
-        for i, a in enumerate(str(num)[::-1]):
-            ans.append(int(a) * int(pow(10, i)))
-        print(ans[::-1])
+        while num > 0:
+            x = len(str(num)) - 1
+            y = int(str(num)[0])
+            
+            nu = y * int(pow(10, x))
+            if nu in self.D:
+                ans.append(self.D[nu])
+            else:
+                if y in [6, 7, 8]:
+                    small_ans = self.D[5*int(pow(10, x))] + self.D[int(pow(10, x))] * (y-5)
+                    ans.append(small_ans)
+                else:
+                    ans.append(self.D[int(pow(10, x))] * y)
+            
+            num -= nu
+        
+        print "".join(ans)
+
 
 
 S = Solution()
-S.intToRoman(567)
+S.intToRoman(199)
